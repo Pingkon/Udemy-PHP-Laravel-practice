07239815fdfce9963ff5f65cfd0f49883cc9ae58
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2023-06-24 16:41:59 +0600

    updating database

--- database/migrations/2023_06_24_100151_create_posts_table.php
+++ database/migrations/2023_06_24_100151_create_posts_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('content');
             $table->text('body');
+            $table->text('content');
             $table->timestamps();
         });
     }
